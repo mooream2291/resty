4d04afa50a0f298631e0e0005938ff76cf5a7bae
Ashley Moore <mooream2291@gmail.com>
2021-01-14 00:03:00 -0500

    updated get and history.js

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -4,15 +4,29 @@ import Header from './header';
 import Form from './form';
 import Footer from './footer';
 import './app.scss';
-import './header.scss'
+import './header.scss';
+import History from './history'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: []
+    }
+  }
+
+  updateHistory = newObj => {
+    //setting the state overwrites anything previous (it does not edit, it complete gets rid of it)
+    this.setState({ history: [...this.state.history, newObj] })
+
+  }
 
   render() {
     return (
       <>
       <Header />
-      <Form />
+      <Form updateHistory= {this.updateHistory}/>
+      <History history= {this.state.history}/>
       <Footer />
       </>
     )
--- react-demo/src/history.js
+++ react-demo/src/history.js
@@ -1,4 +1,24 @@
-'use strict';
+import React from 'react';
 
 //once the routes are successful, they will save the data into local storage and the history component will be pulled out and displayed on the page//
 
+class History extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                this.props.history.map((entry) => {
+                    return (
+                    <p>{entry.method} {entry.url}</p>
+                    )
+                }) 
+                }
+            </div>
+        )
+    }
+}
+export default History;
