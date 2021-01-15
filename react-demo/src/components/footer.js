a4059b8017b8bc519bcbbb8f47ac3e540a40854b
Ashley Moore <mooream2291@gmail.com>
2021-01-15 07:43:02 -0500

    incomplete

--- react-demo/src/components/footer.js
+++ react-demo/src/components/footer.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './footer.scss';
 
-class Footer extends React.Component {
-    render(){
+// class Footer extends React.Component{
+const Footer = props => {
+    render()
       return(
         <footer>&copy; Code Fellows 2018</footer>
-      )
-    }
-  }
+      )};
 
   export default Footer;
--- react-demo/src/components/header.js
+++ react-demo/src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.scss';
+import { NavLink } from 'react-router-dom';
 
 class Header extends React.Component {
     render(){
